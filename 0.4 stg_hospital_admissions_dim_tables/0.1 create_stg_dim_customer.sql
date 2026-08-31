da95052c00a4553827ab59b6880ad67710d80faf
kamohelomolotsi1-svg <kamohelomolotsi1@gmail.com>
2026-08-31 17:12:50 +0200

    Importing_csv_file_and_creating_stg_dim_customer

---
+++ 0.4 stg_hospital_admissions_dim_tables/0.1 create_stg_dim_customer.sql
@@ -0,0 +1,22 @@
+-- select distinct customer details from raw table
+
+SELECT DISTINCT [first_name],
+                [surname],
+                [national_id]
+FROM [stg_hospital_admissions].[dbo].[hospital_admissions_raw]
+
+----------------------------------------------------------------------------
+
+-- 01 create_stg_dim_customer
+
+USE stg_hospital_admissions
+GO
+
+IF OBJECT_ID(N'[stg_hospital_admissions].[dbo].[stg_dim_customer]', N'U') IS NULL
+CREATE TABLE [stg_hospital_admissions].[dbo].[stg_dim_customer] (
+    [Customer_ID] INT IDENTITY(1, 1) PRIMARY KEY,
+    [First_Name] VARCHAR(255),
+    [Surname] VARCHAR(255),
+    [National_ID] INT
+    );
+GO
