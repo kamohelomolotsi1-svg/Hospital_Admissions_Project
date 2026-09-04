00e815f642b0a7df35c125d1d88b640038138156
kamohelomolotsi1-svg <kamohelomolotsi1@gmail.com>
2026-09-04 17:35:11 +0200

    Creating_stg_dim_severity

---
+++ 0.4 stg_hospital_admissions_dim_tables/0.4.4 create_stg_dim_disease.sql
@@ -0,0 +1,17 @@
+-- select distinct severity details from raw table
+
+SELECT DISTINCT [severity]
+FROM [stg_hospital_admissions].[dbo].[hospital_admissions_raw]
+---------------------------------------------------------------------
+
+-- 044 create_stg_dim_disease
+
+USE stg_hospital_admissions
+GO
+
+IF OBJECT_ID(N'[stg_hospital_admissions].[dbo].[stg_dim_severity]', N'U') IS NULL
+CREATE TABLE [stg_hospital_admissions].[dbo].[stg_dim_severity] (
+    [Severity_ID] INT IDENTITY(1, 1) PRIMARY KEY,
+    [Severity] VARCHAR(255)
+    );
+GO
