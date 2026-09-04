102f4d24717aa385295dcaff9c4b5b80ef027806
kamohelomolotsi1-svg <kamohelomolotsi1@gmail.com>
2026-09-04 19:34:14 +0200

    loading_stg_dim_customer

--- 0.4 stg_hospital_admissions_dim_tables/0.4.1 create_stg_dim_customer.sql
+++ 0.4 stg_hospital_admissions_dim_tables/0.4.1 create_stg_dim_customer.sql
@@ -12,11 +12,12 @@ FROM [stg_hospital_admissions].[dbo].[hospital_admissions_raw]
 USE stg_hospital_admissions
 GO
 
+--DROP TABLE IF EXISTS [stg_hospital_admissions].[dbo].[stg_dim_customer]
 IF OBJECT_ID(N'[stg_hospital_admissions].[dbo].[stg_dim_customer]', N'U') IS NULL
 CREATE TABLE [stg_hospital_admissions].[dbo].[stg_dim_customer] (
     [Customer_ID] INT IDENTITY(1, 1) PRIMARY KEY,
     [First_Name] VARCHAR(255),
     [Surname] VARCHAR(255),
-    [National_ID] INT
+    [National_ID] FLOAT
     );
 GO
---
+++ 0.6 load_stg_hospital_admissions/0.6.1 load_stg_dim_customer.sql
@@ -0,0 +1,88 @@
+USE stg_hospital_admissions;
+GO
+
+
+-- 061 load stg dim customer
+--DROP PROCEDURE IF EXISTS dbo.usp_Load_stg_dim_customer
+CREATE PROCEDURE dbo.usp_Load_stg_dim_customer
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    DECLARE @BatchID UNIQUEIDENTIFIER = NEWID();
+    DECLARE @StartTime DATETIME = GETDATE();
+    DECLARE @RowsInserted INT;
+
+    BEGIN TRY
+
+        INSERT INTO [stg_hospital_admissions].[dbo].[stg_dim_customer]
+        (
+            [First_Name],
+            [Surname],
+            [National_ID]
+        )
+        SELECT DISTINCT
+            [First_Name],
+            [Surname],
+            [National_ID]
+        FROM [stg_hospital_admissions].[dbo].[hospital_admissions_raw];
+
+        SET @RowsInserted = @@ROWCOUNT;
+
+        INSERT INTO [stg_hospital_admissions].[dbo].[etl_audit_log]
+        (
+            batch_id,
+            procedure_name,
+            start_time,
+            end_time,
+            rows_inserted,
+            status,
+            error_message
+        )
+        VALUES
+        (
+            @BatchID,
+            'usp_Load_stg_dim_customer',
+            @StartTime,
+            GETDATE(),
+            @RowsInserted,
+            'SUCCESS',
+            NULL
+        );
+
+    END TRY
+
+    BEGIN CATCH
+
+        INSERT INTO [stg_hospital_admissions].[dbo].[etl_audit_log]
+        (
+            batch_id,
+            procedure_name,
+            start_time,
+            end_time,
+            rows_inserted,
+            status,
+            error_message
+        )
+        VALUES
+        (
+            @BatchID,
+            'usp_Load_stg_dim_customer',
+            @StartTime,
+            GETDATE(),
+            0,
+            'FAILED',
+            ERROR_MESSAGE()
+        );
+
+        THROW;
+
+    END CATCH;
+
+END;
+GO
+
+----------------------------------------------------------
+
+EXEC dbo.usp_Load_stg_dim_customer
