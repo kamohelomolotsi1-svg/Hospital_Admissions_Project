c84dde8f89a797cad745d575e6242e37ff336556
kamohelomolotsi1-svg <kamohelomolotsi1@gmail.com>
2026-09-05 19:08:12 +0200

    Audit_log_clean_dim

---
+++ 0.5 stg_hospital_admissions_audit_log/0.5.2 create_audit_log_clean_table.sql
@@ -0,0 +1,16 @@
+-- creating audit log table for clean hospital admissions
+
+USE clean_hospital_admissions;
+GO
+
+CREATE TABLE [clean_hospital_admissions].[dbo].[etl_audit_log]
+(
+    audit_id INT IDENTITY(1,1) PRIMARY KEY,
+    batch_id UNIQUEIDENTIFIER,
+    procedure_name VARCHAR(255),
+    start_time DATETIME,
+    end_time DATETIME,
+    rows_inserted INT,
+    status VARCHAR(20),
+    error_message NVARCHAR(MAX)
+);
