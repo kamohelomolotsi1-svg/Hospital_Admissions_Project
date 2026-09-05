6f5d6a572fb22653d5fdc8889df14f40ebc45a7d
kamohelomolotsi1-svg <kamohelomolotsi1@gmail.com>
2026-09-05 17:46:08 +0200

    Creating_clean_dim_date

---
+++ 0.7 clean_hospital_admissions_dim_tables/0.7.2 create_clean_dim_date.sql
@@ -0,0 +1,25 @@
+-- 072 create_stg_dim_date
+
+USE clean_hospital_admissions
+GO
+
+--DROP TABLE IF EXISTS [stg_hospital_admissions].[dbo].[stg_dim_date]
+
+IF OBJECT_ID('[clean_hospital_admissions].[dbo].[clean_dim_date]', 'U') IS NULL
+BEGIN
+    CREATE TABLE [clean_hospital_admissions].[dbo].[clean_dim_date]
+    (
+        [date_id] INT IDENTITY(1, 1) PRIMARY KEY,
+        [date_key] INT,
+        [full_date] DATE,
+        [day_number] TINYINT,
+        [day_name] VARCHAR(20),
+        [month_number] TINYINT,
+        [month_name] VARCHAR(20),
+        [quarter_number] TINYINT,
+        [year_number] SMALLINT,
+        [week_number] TINYINT,
+        [day_of_week] TINYINT
+    );
+END;
+GO
