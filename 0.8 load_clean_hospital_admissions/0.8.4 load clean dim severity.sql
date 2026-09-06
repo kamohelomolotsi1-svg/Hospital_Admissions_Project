adff4dfe2f6ed4c3f07f995289014beb52b1b2f5
kamohelomolotsi1-svg <kamohelomolotsi1@gmail.com>
2026-09-06 15:20:29 +0200

    Load_clean_dim_severity

---
+++ 0.8 load_clean_hospital_admissions/0.8.4 load clean dim severity.sql
@@ -0,0 +1,84 @@
+USE clean_hospital_admissions;
+GO
+
+
+-- 084 load clean dim severity
+
+CREATE PROCEDURE dbo.usp_Load_clean_dim_severity
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    DECLARE @BatchID UNIQUEIDENTIFIER = NEWID();
+    DECLARE @StartTime DATETIME = GETDATE();
+    DECLARE @RowsInserted INT;
+
+    BEGIN TRY
+
+        INSERT INTO [clean_hospital_admissions].[dbo].[clean_dim_severity]
+        (
+            [Severity]
+        )
+        SELECT DISTINCT
+          [Severity]
+        FROM [stg_hospital_admissions].[dbo].[stg_dim_severity];
+
+        SET @RowsInserted = @@ROWCOUNT;
+
+        INSERT INTO [clean_hospital_admissions].[dbo].[etl_audit_log]
+        (
+            batch_id,
+            procedure_name,
+            start_time,
+            end_time,
+            rows_inserted,
+            status,
+            error_message
+        )
+        VALUES
+        (
+            @BatchID,
+            'usp_Load_clean_dim_severity',
+            @StartTime,
+            GETDATE(),
+            @RowsInserted,
+            'SUCCESS',
+            NULL
+        );
+
+    END TRY
+
+    BEGIN CATCH
+
+        INSERT INTO [clean_hospital_admissions].[dbo].[etl_audit_log]
+        (
+            batch_id,
+            procedure_name,
+            start_time,
+            end_time,
+            rows_inserted,
+            status,
+            error_message
+        )
+        VALUES
+        (
+            @BatchID,
+            'usp_Load_clean_dim_severity',
+            @StartTime,
+            GETDATE(),
+            0,
+            'FAILED',
+            ERROR_MESSAGE()
+        );
+
+        THROW;
+
+    END CATCH;
+
+END;
+GO
+
+--------------------------------------------------------------------
+
+EXEC dbo.usp_Load_clean_dim_severity
